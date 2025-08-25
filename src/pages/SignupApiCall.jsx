0ed7e025f3c1954150206083804aefe598ae9760
Maulik-khambhdiya <maulikkhambhdiya444@gmail.com>
2025-08-25 17:54:30 +0530

    signup api call

--- src/pages/SignupApiCall.jsx
+++ src/pages/SignupApiCall.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Container,
     TextField,
-    Button,
     Typography,
     Box,
     Paper,
-    Stack,
+
 } from '@mui/material';
 import { Field, Form, Formik } from 'formik';
 import axios from 'axios';
@@ -14,7 +13,7 @@ import axios from 'axios';
 
 const SignupApiCall = () => {
 
-
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YTcwNTk0YTE0OWFmMjY1OTBjN2E5NyIsImlhdCI6MTc1NTc3NjU2Nn0.gqEW6zR7Guo9As3AAY_nwSaVCbHuz8KOFbkLTlVweg8"
 
     const [list, setList] = useState([])
     const [ini, setIni] = useState({
@@ -23,21 +22,98 @@ const SignupApiCall = () => {
         password: "",
         profile: ""
     })
+    const [editId, setEditId] = useState(null)
 
 
+    useEffect(() => {
+        viewData()
+    }, [])
 
-    const handleSubmit = (vaules) => {
-
-        axios.post("localhost:3000/signup", vaules)
+    const viewData = () => {
+        axios.get("http://localhost:3000/signup", {
+            headers: {
+                Authorization: token
+            }
+        })
             .then((res) => {
-                console.log(res.data);
+
+                setList(res.data.data)
+                //console.log(res.data.data);
+
 
             })
             .catch((error) => {
                 console.log(error);
 
             })
+    }
 
+
+    const handleSubmit = (vaules, { resetForm }) => {
+
+        const { _id, ...rest } = vaules
+
+
+        if (editId != null) {
+            axios.patch(`https://localhost:3000/signup/${editId}`, rest, {
+                headers: {
+                    Authorization: token
+                }
+            })
+                .then((res) => {
+                    viewData()
+                    setEditId(null)
+                    setIni({
+                        name: "",
+                        email: "",
+                        password: "",
+                        profile: ""
+
+                    })
+                })
+        }
+
+        else {
+            axios.post("http://localhost:3000/signup", vaules, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(() => {
+                    console.log("Data added successfully");
+
+                    resetForm()
+                    viewData()
+                })
+
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+
+
+    }
+
+    const deleteData = (deleteId) => {
+        axios.delete(`http://localhost:3000/signup/${deleteId}`, {
+            headers: {
+                Authorization: token
+            }
+        })
+            .then((res) => {
+                console.log("Data Deleted Successfully");
+                viewData()
+
+            })
+            .catch((error) => {
+                console.log(error);
+
+            })
+    }
+
+    const editData = (item) => {
+        setIni(item)
+        setEditId(item._id)
     }
 
 
@@ -72,66 +148,113 @@ const SignupApiCall = () => {
                             initialValues={ini}
                             onSubmit={handleSubmit}
                         >
-                            <Form>
 
-                                <Field name="name"
-                                    as={TextField}
-                                    label="Name"
-                                    fullWidth
-                                    variant="outlined"
-                                    margin="normal"></Field><br />
+                            {({ setFieldValue }) => (
+                                <Form encType='multipart/form-data'>
 
-
-                                <Field name="email"
-                                    as={TextField}
-                                    label="Email"
-                                    fullWidth
-                                    variant="outlined"
-                                    margin="normal"></Field><br />
+                                    <Field name="name"
+                                        as={TextField}
+                                        label="Name"
+                                        fullWidth
+                                        variant="outlined"
+                                        margin="normal"></Field><br />
 
 
-
-                                <Field name="password"
-                                    as={TextField}
-                                    label="Password"
-                                    fullWidth
-                                    variant="outlined"
-                                    margin="normal"></Field><br />
-
-
-                                <Field name="profile"
-
-                                    type="file"
-
-                                    label="Profile"
-                                    fullWidth
-                                    variant="outlined"
-                                    margin="normal"></Field><br /><br />
-
-                                <button type="submit" fullWidth
+                                    <Field name="email"
+                                        as={TextField}
+                                        label="Email"
+                                        fullWidth
+                                        variant="outlined"
+                                        margin="normal"></Field><br />
 
 
-                                    style={{
-                                        backgroundColor: '#0d47a1',
-                                        color: '#fff',
-                                        padding: "10px 30px",
-                                        border: "none",
-                                        borderRadius: "5px",
-                                        fontSize: "20px",
 
-                                    }}
-
-                                >Create</button>
+                                    <Field name="password"
+                                        as={TextField}
+                                        label="Password"
+                                        fullWidth
+                                        variant="outlined"
+                                        margin="normal"></Field><br />
 
 
-                            </Form>
+                                    <label htmlFor="profile" style={{
+                                        display: 'flex',
+                                        padding: '15px 20px',
+                                        backgroundColor: '#1976d2',
+                                        color: 'white',
+                                        borderRadius: '4px',
+                                        cursor: 'pointer',
+                                        marginTop: '10px',
+
+                                    }}>
+                                        Upload Profile
+                                    </label>
+
+                                    <input
+                                        type="file"
+                                        name="profile"
+                                        id="profile"
+                                        style={{ display: 'none' }}
+                                        onChange={(e) => setFieldValue("profile", e.target.files[0])}
+                                    />
+                                    <br /><br />
+
+                                    <button type="submit" fullWidth
+
+
+                                        style={{
+                                            backgroundColor: '#0d47a1',
+                                            color: '#fff',
+                                            padding: "10px 30px",
+                                            border: "none",
+                                            borderRadius: "5px",
+                                            fontSize: "20px",
+
+                                        }}
+
+                                    >Create</button>
+
+
+                                </Form>
+                            )}
+
                         </Formik>
+
 
                     </Box>
 
 
                 </Paper>
+
+
             </Container>
+
+            <table border={1}>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Profile</th>
+                    <th>Delete</th>
+                </tr>
+
+                {
+                    list.map((item, index) => (
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td>{item.name}</td>
+                            <td>{item.email}</td>
+                            <td>{item.password}</td>
+                            <td>{item.profile}</td>
+                            <td><button onClick={() => deleteData(item._id)}>Delete</button></td>
+                            <td><button onClick={() => editData(item)}>Edit</button></td>
+
+                        </tr>
+                    ))
+                }
+            </table>
+
 
         </>
     )
